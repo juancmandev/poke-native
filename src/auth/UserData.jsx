3c119caf7c550f432b987f8632baf3c2a70a97c4
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-06-28 20:38:51 -0500

    add pokemon favorites count on account screen

--- src/auth/UserData.jsx
+++ src/auth/UserData.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import useAuth from '../hooks/useAuth';
+import { getPokemonFavoriteApi } from '../api/favorites';
 
 export default function UserData() {
   const { auth, logout } = useAuth();
+  const [total, setTotal] = useState(0);
+
+  useFocusEffect(
+    useCallback(() => {
+      (async () => {
+        try {
+          const response = await getPokemonFavoriteApi();
+          setTotal(response.length);
+        } catch (erroo) {
+          setTotal(0);
+        }
+      })();
+    }, [])
+  );
 
   return (
     <View style={styles.content}>
@@ -16,7 +32,7 @@ export default function UserData() {
         <ItemMenu title='Name' text={`${auth.firstName} ${auth.lastName}`} />
         <ItemMenu title='Username' text={`${auth.username}`} />
         <ItemMenu title='Email' text={`${auth.email}`} />
-        <ItemMenu title='Favorites count' text='0 Pokémon' />
+        <ItemMenu title='Favorites count' text={`${total} Pokémon`} />
       </View>
 
       <Button title='Logout' onPress={logout} />
